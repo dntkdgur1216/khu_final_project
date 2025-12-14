bac75ab166709aea8db2b2aa2979b7eaa3e2512f
dntkdgur1216 <87884854+dntkdgur1216@users.noreply.github.com>
2025-12-15 02:25:51 +0900

    Clean up run_lfcbm_remoteclip_eurosat_real.sh
    Removed a comment regarding node specification errors and cleaned up the script.

--- run_lfcbm_remoteclip_eurosat_real.sh
+++ run_lfcbm_remoteclip_eurosat_real.sh
@@ -10,6 +10,7 @@
 #SBATCH -o logs/lfcbm_remoteclip_eurosat-%A.out
 #SBATCH -e logs/lfcbm_remoteclip_eurosat-%A.err
 
+# 11월 중반부터, 실행되는 컴퓨팅 노드를 지정하면 오류가 생기기 시작했다. 그래서 제거함
 set -e
 
 echo "Job Started on $(hostname)"
@@ -19,7 +20,6 @@ cd $PROJECT_DIR
 echo "Changed directory to $(pwd)"
 mkdir -p logs saved_activations_remoteclip saved_models_remoteclip data/concept_sets
 
-# --- 메인 명령어 실행 ---
 echo "Running LF-CBM Training with RemoteCLIP..."
 
 # 무슨 버그지? 일단 한줄로 하자
