55078b6189f6df54e30cf10d7990c58559bc5905
dntkdgur1216 <87884854+dntkdgur1216@users.noreply.github.com>
2025-12-15 02:23:52 +0900

    Fix command execution for LF-CBM training
    Updated the command to run LF-CBM training with RemoteCLIP, ensuring all arguments are on one line to prevent line break issues.

--- run_lfcbm_remoteclip_eurosat_real.sh
+++ run_lfcbm_remoteclip_eurosat_real.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-#SBATCH --job-name=train_lf-cbm_remoteclip      # 작업 이름
+#SBATCH --job-name=train_lf-cbm_remoteclip      
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-gpu=8
 #SBATCH --mem-per-gpu=29G
 #SBATCH -p batch_ce_ugrad
 
-#SBATCH -t 1-0                                  # 시간 제한: 1일
+#SBATCH -t 1-0                                  
 #SBATCH -o logs/lfcbm_remoteclip_eurosat-%A.out
 #SBATCH -e logs/lfcbm_remoteclip_eurosat-%A.err
 
 set -e
 
-# --- 실행 환경 설정 ---
 echo "Job Started on $(hostname)"
 PROJECT_DIR="/data/dntkdgur1216/repos/final_project/Label-free-CBM"
 source /data/dntkdgur1216/anaconda3/etc/profile.d/conda.sh
@@ -23,7 +22,7 @@ mkdir -p logs saved_activations_remoteclip saved_models_remoteclip data/concept_
 # --- 메인 명령어 실행 ---
 echo "Running LF-CBM Training with RemoteCLIP..."
 
-# [핵심 수정] 모든 인자를 한 줄로 합쳐서 줄 바꿈 오류를 방지합니다.
-conda run -n lf_remote_env python train_cbm.py --dataset eurosat_rgb --backbone "remote_clip_vit_b_32" --clip_name 'ViT-B-32' --remote_clip_path "../checkpoints/models--chendelong--RemoteCLIP/snapshots/bf1d8a3ccf2ddbf7c875705e46373bfe542bce38/RemoteCLIP-ViT-B-32.pt" --concept_set "data/concept_sets/eurosat_concepts4.txt" --activation_dir "saved_activations_remoteclip" --save_dir "saved_models_remoteclip" --device cuda
+# 무슨 버그지? 일단 한줄로 하자
+conda run -n lf_remote_env python train_cbm.py --dataset eurosat_rgb --backbone "remote_clip_vit_b_32" --clip_name 'ViT-B-32' --remote_clip_path "../checkpoints/models--chendelong--RemoteCLIP/snapshots/bf1d8a3ccf2ddbf7c875705e46373bfe542bce38/RemoteCLIP-ViT-B-32.pt" --concept_set "data/concept_sets/eurosat_concepts2.txt" --activation_dir "saved_activations_remoteclip" --save_dir "saved_models_remoteclip" --device cuda
 
 echo "Job Finished"
